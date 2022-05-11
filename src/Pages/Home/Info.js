704eadb4d83a5947c2c7088dc20967506e49abf0
mirajul alam <alifmirajulalam@gmail.comm>
2022-05-11 11:58:28 +0600

    appointment page footer added

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -7,7 +7,7 @@ const Info = () => {
     return (
         <div className='grid sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-5'>
             <InfoCard cardTitle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary" cardDescription="Time is very importent for a doctor" img={clock}></InfoCard>
-            <InfoCard cardTitle="Visit Our Location" bgClass="bg-accent" cardDescription="Bangladesh,Chittagong,fatikchari" img={marker}></InfoCard>
+            <InfoCard cardTitle="Visit Our Location" bgClass="bg-[#3A4256]" cardDescription="Bangladesh,Chittagong,fatikchari" img={marker}></InfoCard>
             <InfoCard cardTitle="Contact Us" bgClass="bg-gradient-to-r from-secondary to-primary" cardDescription="+01643760528" img={phone}></InfoCard>
         </div>
     );
