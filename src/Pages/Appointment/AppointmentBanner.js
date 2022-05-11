295695862143482d705de22c710beb7a468ba95c
mirajul alam <alifmirajulalam@gmail.comm>
2022-05-11 12:20:06 +0600

    appointment page banner added

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chair1 from '../../assets/images/chair.png';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { format } from 'date-fns';
+import bg from '../../assets/images/bg.png';
 const AppointmentBanner = () => {
+    const [date, setDate] = useState(new Date())
     return (
-        <div class="hero min-h-screen">
+        <div style={{
+            background: `url(${bg})`,
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat'
+        }} class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair1} class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <DayPicker
+                        mode='single'
+                        selected={date}
+                        onSelect={setDate}
+                    />
+                    <p>You have selected: {format(date, "PP")}</p>
                 </div>
             </div>
         </div>
