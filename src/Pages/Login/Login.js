f3b6cf7bfd4487470b37ac097dd6636e0c37a5d2
mirajul alam <alifmirajulalam@gmail.comm>
2022-05-11 22:24:29 +0600

    social login google implement added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,37 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    if (user) {
+        console.log(user);
+    }
     return (
-        <div class="hero min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div class="card-body">
-                        <h1 className='text-center font-bold text-xl'>Login</h1>
-                        <div class="form-control">
-                            <label class="label">
-                                <span class="label-text">Email</span>
-                            </label>
-                            <input type="text" placeholder="email" class="input input-bordered" />
-                        </div>
-                        <div class="form-control">
-                            <label class="label">
-                                <span class="label-text">Password</span>
-                            </label>
-                            <input type="text" placeholder="password" class="input input-bordered" />
-                            <label class="label">
-                                <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div class="form-control mt-6">
-                            <button class="btn btn-accert text-white">Login</button>
-                        </div>
-                        <p>New to Doctor Portal? <Link className='text-primary' to='/login'>Create new account</Link></p>
-                        <div class="flex flex-col w-full border-opacity-50">
-                            <div class="divider">OR</div>
-                        </div>
-                        <input className='btn border-1-black-100' type="submit" value="submit" />
-                    </div>
+        <div className='flex h-screen justify-center items-center'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center font-bold text-2xl">Login</h2>
+                    <div className="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        className="btn btn-outline">Continue with Google</button>
                 </div>
             </div>
         </div>
