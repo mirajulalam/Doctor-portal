2caa9ed641a57731dc9b882c73288eafe30a2aad
mirajul alam <alifmirajulalam@gmail.comm>
2022-05-14 21:21:50 +0600

    spaces available update added

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -6,15 +6,17 @@ import Service from './Service';
 const AvailableAppointment = ({ date }) => {
     const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
+
+    const formattedDate = format(date, "PP");
     useEffect(() => {
-        fetch('http://localhost:5000/service')
+        fetch(`http://localhost:5000/available?date=${formattedDate}`)
             .then(res => res.json())
             .then(data => setServices(data))
     }, [])
     return (
         <div>
             <h4 className='text-xl text-center text-secondary font-bold my-20'>Available Appointments on {format(date, "PP")}</h4>
-            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-20'>
                 {
                     services.map(service => <Service
                         key={service._id}
